9132dfa168a2b7116152cdf4f6dc89dd09650339
bhijanbastola <bhjanbastola@gmail.com>
2026-01-19 21:42:54 +0545

    first commit

---
+++ reserve/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+def base(request):
+    return render(request, 'base.html')
+
+
+
